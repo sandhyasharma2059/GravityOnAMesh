7775a8eea4e3b3e61bb14bd8de4f70c9dec25a9c
Zion <jli21bh@gmail.com>
2023-10-26 17:43:24 -0400

    converted to a dictionary

--- functions.py
+++ functions.py
@@ -24,39 +24,51 @@ ax.set_zlabel('Z')
 
 plt.show()
 
+particles = np.column_stack(x,y,z]
+
 def compute_density_field(particles, grid_res=32):
 
-    min_coords = particles.min(axis=0) - 0.5  # accounting for the 1x1x1 cube around the particle
+    min_coords = particles.min(axis=0) - 0.5  
     max_coords = particles.max(axis=0) + 0.5
     cell_size = (max_coords - min_coords) / grid_res
     
-    # Initialize the density field to zeros
     density = np.zeros((grid_res, grid_res, grid_res))
     
-    # Iterate over all grid cells
+    cell_particle_map = {}
+
+    for p in particles:
+        particle_min = p - 0.5
+        particle_max = p + 0.5
+
+        cell_min_idx = np.floor((particle_min - min_coords) / cell_size).astype(int)
+        cell_max_idx = np.ceil((particle_max - min_coords) / cell_size).astype(int)
+
+        for i in range(cell_min_idx[0], cell_max_idx[0]):
+            for j in range(cell_min_idx[1], cell_max_idx[1]):
+                for k in range(cell_min_idx[2], cell_max_idx[2]):
+                    if (i, j, k) not in cell_particle_map:
+                        cell_particle_map[(i, j, k)] = []
+                    cell_particle_map[(i, j, k)].append(p)
+                    
     for i in range(grid_res):
         for j in range(grid_res):
             for k in range(grid_res):
-                # Determine the spatial extent of this cell
                 cell_min = min_coords + cell_size * np.array([i, j, k])
                 cell_max = cell_min + cell_size
                 
-                # Check overlap with each particle's cube and sum contributions
-                for p in particles:
+                relevant_particles = cell_particle_map.get((i, j, k), [])
+                for p in relevant_particles:
                     particle_min = p - 0.5
                     particle_max = p + 0.5
                     
-                    # Compute the overlapping volume between the grid cell and particle cube
                     overlap_min = np.maximum(cell_min, particle_min)
                     overlap_max = np.minimum(cell_max, particle_max)
                     overlap_size = np.maximum(0, overlap_max - overlap_min)
                     overlap_volume = overlap_size[0] * overlap_size[1] * overlap_size[2]
                     
-                    # Add the overlap volume to the density of this cell
                     density[i, j, k] += overlap_volume
                     
     return density
 
-# Compute the density field for the given particles
 density_field = compute_density_field(particles)
 
