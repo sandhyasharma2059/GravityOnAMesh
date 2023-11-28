90de6b7773bae89f3448f321d006faa817c1d224
Zion <jli21bh@gmail.com>
2023-11-28 16:40:33 -0500

    resolved changes

--- functions.py
+++ functions.py
@@ -101,26 +101,40 @@ def compute_density_field(particles, grid_res=32):
 
 density_field = compute_density_field(particles)
 
-#plotting the density field
-x, y, z = np.meshgrid(np.arange(density_field.shape[0]), np.arange(density_field.shape[1]), np.arange(density_field.shape[2]))
+def plot_2d_slice(phi, name, axis='z', slice_index=None):
+    """
+    Plot a 2D slice of the 3D quantity.
+    
+    Parameters:
+    - phi: 3D numpy array of quantity values.
+    - axis: The axis to take the slice along ('x', 'y', or 'z').
+    - slice_index: The index of the slice along the chosen axis. If None, it defaults to the middle of the grid.
+    """
+    N = phi.shape[0]
+    
+    if slice_index is None:
+        slice_index = N // 2
+    
+    if axis == 'x':
+        slice_2d = phi[slice_index, :, :]
+    elif axis == 'y':
+        slice_2d = phi[:, slice_index, :]
+    elif axis == 'z':
+        slice_2d = phi[:, :, slice_index]
+    else:
+        raise ValueError("Invalid axis. Choose 'x', 'y', or 'z'.")
+    
+    plt.figure(figsize=(8, 6))
+    plt.imshow(slice_2d, extent=[0, N, 0, N], origin='lower', cmap='viridis')
+    plt.colorbar(label=name)
+    plt.xlabel(f'{axis} = {slice_index}')
+    plt.ylabel('Grid Index')
+    plt.title(f'2D Slice along {axis}-axis')
+    plt.show()
 
-print(x.shape, y.shape, z.shape)
+    return 
 
-x = x.flatten()
-y = y.flatten()
-z = z.flatten()
-
-density_values = density_field.flatten()
-
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection='3d')
-sc = ax.scatter(x, y, z, c=density_values, cmap='viridis', vmin=15, vmax=25, alpha = 0.1)
-plt.colorbar(sc, label='Density')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.title('3D Density Plot')
-plt.show()
+plot_2d_slice(density_field, 'density', 'z')
 
 # SOLVING THE POISSON EQUATION
 def solve_poisson_fft(density_field):
@@ -172,39 +186,33 @@ def plot_potential(phi, title):
         Returns:
         A plot of the potential in the form of a 3D scatter plot.
     '''
+def plot_potential_vs_radius(phi):
     N = phi.shape[0]
-    x, y, z = np.indices((N, N, N))
+    center = N // 2
 
-    fig = plt.figure(figsize=(10, 8))  
-    ax = fig.add_subplot(111, projection='3d')
+    x, y, z = np.indices((N, N, N)) - center
+    r = np.sqrt(x**2 + y**2 + z**2)
 
-    x = x.flatten()
-    y = y.flatten()
-    z = z.flatten()
-    phi = phi.flatten()
+    r_flat = r.flatten()
+    phi_flat = phi.flatten()
 
-    # Normalizing potential values for color mapping and transparency
-    phi_norm = (phi - phi.min()) / (phi.max() - phi.min())
-    cmap = plt.cm.viridis
-    # Points with phi_norm <= 0.1 are almost invisible
-    alpha = np.where(phi_norm > 0.05, 1, 0)  
+    unique_r = np.unique(r_flat)
+    average_phi = np.array([phi_flat[r_flat == radius].mean() for radius in unique_r])
 
-    sc = ax.scatter(x, y, z, c=phi_norm, cmap=cmap, alpha=alpha, marker='o', s=10)
-
-    plt.colorbar(sc)
-
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_title(title)
-
+    # Plotting
+    plt.figure(figsize=(8, 6))
+    plt.plot(unique_r, average_phi, marker='o')
+    plt.plot(unique_r, 1 / unique_r, label='1/r', color='red')
+    plt.xlabel('Radius (r)')
+    plt.ylabel('Average Potential (phi)')
+    plt.title('Average Potential vs. Radius')
+    plt.grid(True)
     plt.show()
-    return 
 
 #solving the Poisson equation for a delta source
 point_source = delta_source(32)
 phi_point_source = solve_poisson_fft(point_source)
-plot_potential(phi_point_source, 'Potential for a Delta Source')
+plot_potential_vs_radius(phi_point_source)
 
 def green_function(N):
     '''
@@ -222,9 +230,9 @@ def green_function(N):
     X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
 
     r = np.sqrt(X**2 + Y**2 + Z**2)
-    g = np.where((np.abs(X) < 1) & (np.abs(Y) < 1) & (np.abs(Z) < 1), 1/r, 0)
-    g[N//2, N//2, N//2] = 1  
-
+    r[N//2, N//2, N//2] = 1
+    g = np.where(r <= N//2, 1/r, 0) 
+    g[0,0,0] = 1 
     return g
 
 def solve_poisson_green(density, g):
@@ -247,7 +255,7 @@ def solve_poisson_green(density, g):
     return phi
 
 N = 64
-point_source = delta_source(64)
-g = green_function(64)
+point_source = delta_source(N)
+g = green_function(N)
 phi = solve_poisson_green(point_source, g)
-plot_potential(phi, 'Potential for a Delta Source using Green Function') 
+plot_potential_vs_radius(phi)
